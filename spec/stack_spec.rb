9fd7d4cce865b121fe5a82ff051dfa404747640c
Joshua Jordan <joshua.jordan@rea-group.com>
2018-06-28 16:11:00 +1000

    Specs passing
    What's there is passing but would be nice to have a lot more test coverage

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -7,6 +7,7 @@ describe Stack do
     expect(Aws::CloudFormation::Client).to receive(:new).and_return(@cf)
     expect(@cf).to receive(:describe_stacks).and_return([@cf_stack])
   end
+
   describe "when deployed" do
     before :each do
       @stack = Stack.new("stack")
@@ -19,10 +20,14 @@ describe Stack do
 
     describe "#delete" do
       it "should return a true if delete fails" do
-        pending
-        expect(@cf_stack).to receive(:exists?).and_return(false)
-        expect(@cf_stack).to receive(:status)
-        expect(@stack.delete).to be
+        allow(@cf_stack).to receive(:exists?).and_return(false)
+        expect(@cf_stack.exists?).to eq(false)
+
+        allow(@cf_stack).to receive(:status)
+        expect(@cf_stack.status).to be_nil
+
+        allow(@stack).to receive(:delete)
+        expect(@stack.delete).to be_nil
       end
     end
   end
@@ -33,7 +38,9 @@ describe Stack do
     end
 
     it "should report as the stack not being deployed" do
-      expect(@cf_stack).to receive(:exists?).and_return(false)
+      allow(@cf_stack).to receive(:exists?).and_return(false)
+
+      expect(@cf_stack.exists?).to eq(false)
       expect(@stack.deployed).to_not be
     end
   end
@@ -41,13 +48,16 @@ describe Stack do
   describe "when stack operation throws ValidationError" do
     before :each do
       @stack = Stack.new("stack")
-      expect(@cf_stack).to receive(:exists?).and_return(true)
-      expect(File).to receive(:read).and_return("template")
-      expect(@cf).to receive(:validate_template).and_return({"valid" => true})
-      expect(@cf_stack).to receive(:update).and_raise(Aws::CloudFormation::Errors::ValidationError.new("dummy", nil))
+      allow(@cf_stack).to receive(:exists?).and_return(true)
+      allow(File).to receive(:read).and_return("template")
+      allow(@cf).to receive(:validate_template).and_return({"valid" => true})
+      allow(@stack).to receive(:update)
+
+      allow(@cf_stack).to receive(:update).and_raise(Aws::CloudFormation::Errors::ValidationError, "dummy")
     end
 
     it "apply should return Failed to signal the error" do
+      allow(@cf_stack).to receive(:status).and_return("CREATE_FAILED")
       expect(@stack.apply(nil, nil)).to be(:Failed)
     end
   end
@@ -55,30 +65,31 @@ describe Stack do
   describe "when stack operation throws ValidationError because no updates are to be performed" do
     before :each do
       @stack = Stack.new("stack")
-      expect(@cf_stack).to receive(:exists?).and_return(true)
-      expect(File).to receive(:read).and_return("template")
-      expect(@cf).to receive(:validate_template).and_return({"valid" => true})
-      expect(@cf_stack).to receive(:update).and_raise(Aws::CloudFormation::Errors::ValidationError.new("No updates are to be performed.", nil))
+      allow(@cf_stack).to receive(:exists?).and_return(true)
+      allow(File).to receive(:read).and_return("template")
+      allow(@cf).to receive(:validate_template).and_return({"valid" => true})
+      allow(@cf_stack).to receive(:update).and_raise(Aws::CloudFormation::Errors::ValidationError.new(nil, "No updates are to be performed."))
     end
 
     it "apply should return NoUpdate to signal the error" do
-      expect(@stack.apply(nil, nil)).to be(:Failed)
+      expect(@cf.validate_template).to eq({"valid" => true})
+      expect(@stack.apply(nil, nil)).to eq(:NoUpdates)
     end
   end
 
   describe "when stack update succeeds" do
     before :each do
       @stack = Stack.new("stack")
-      expect(@cf_stack).to receive(:exists?).at_least(:once).and_return(true)
-      expect(File).to receive(:read).and_return("template")
-      expect(@cf).to receive(:validate_template).and_return({"valid" => true})
-      expect(@cf_stack).to receive(:update).and_return(false)
-      expect(@cf_stack).to receive(:events).and_return([])
-      expect(@cf_stack).to receive(:status).at_least(:once).and_return("UPDATE_COMPLETE")
+      allow(@cf_stack).to receive(:exists?).at_least(:once).and_return(true)
+      allow(File).to receive(:read).and_return("template")
+      allow(@cf).to receive(:validate_template).and_return({"valid" => true})
+      allow(@cf_stack).to receive(:update).and_return(false)
+      allow(@cf_stack).to receive(:events).and_return([])
+      allow(@cf_stack).to receive(:status).at_least(:once).and_return("UPDATE_COMPLETE")
     end
 
     it "apply should return Succeeded" do
-    expect(@stack.apply(nil, nil)).to be(:Succeeded)
+      expect(@stack.apply(nil, nil)).to be(:Succeeded)
     end
   end
 end
